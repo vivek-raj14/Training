376fc621ee2037563230411cc7a984e10b6491d5
Vivek <vivek14.vr@gmail.com>
2026-01-19 11:28:49 +0530

    day3

---
+++ day3/my-app/src/App.js
@@ -0,0 +1,18 @@
+import Header from "../src/components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        Learn React
+      </header>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ day3/my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header">Header</h1>
+    </div>
+  );
+};
+
+export default Header;
